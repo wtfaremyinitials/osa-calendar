ecbcb4f28d138999f7d697528cb63ea22ceeabb2
Will Franzen <wtfranzen@gmail.com>
2015-01-03 16:42:52 -0600

    Implement

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 var osa = require('osa');
+
+var Calendar = {};
+
+Calendar.getCalendars = function(cb) {
+    osa(function() {
+        var Calendar = Application('Calendar');
+        var $calendars = Calendar.calendars;
+
+        var calendars = [];
+
+        for(var i=0; i<$calendars.length; i++) {
+            calendars[i] = {
+                name:        $calendars[i].name(),
+                uid:         $calendars[i].uid(),
+                description: $calendars[i].description(),
+            };
+        }
+
+        return calendars;
+    }, cb);
+};
+
+module.exports = Calendar;
